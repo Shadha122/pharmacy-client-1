744c91ca588b2a6467fbb29901809cad7042b9c3
Shadha122 <16J21194@utas.edu.om>
2024-12-22 20:25:19 +0400

    Update Login.js

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://pharmacy-server-new.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
